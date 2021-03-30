0213922b4b316fba917d3f6c5eda7f379d35ac1b
angellee177 <angelriapurnamasari17@gmail.com>
2021-03-30 11:50:22 +0800

    add: another test case, output must be string

--- multiplyArray/index.js
+++ multiplyArray/index.js
@@ -1,14 +1,26 @@
 const multiplyArray = (val) => {
     // please write your solution here
+    return val.reduce((a, b) => a * b).toString();
 }
 
 
-const testCase = (value = Array, testNumber, expectedResult) => {
-    return console.log(testNumber == expectedResult 
-        ? `multiply ${value} is equivalent with ${expectedResult}` 
-        : `Expected ${expectedResult} but got ${testNumber}`);
+const testCaseCheckValue = (value = Array(), testValue, expectedResult) => {
+    return console.log(testValue == expectedResult 
+        ? `Test case 1: Success! multiply ${value} is equivalent with ${expectedResult}` 
+        : `Test case 1: Failed! Expected ${expectedResult} but got ${testValue}`);
 }
 
-testCase([1,2,3], multiplyArray([1,2,3]), 6);
-testCase([4,8,2,9], multiplyArray([4,8,2,9]), 576);
-testCase([15,210,32], multiplyArray([15,210,32]), 100800);
+const testCaseOutputMustBeString = (value = Array(), testValue, expectedResult = String) => {
+    return console.log(typeof(testValue) === typeof(expectedResult) 
+        ? `Test Case 2: Success! multiply ${value} result type must be a string` 
+        : `Test Case 2: Failed! Expected output type is string but got ${typeof(testValue)}`);
+}
+
+// Test case 1
+testCaseCheckValue([1,2,3], multiplyArray([1,2,3]), '6');
+testCaseCheckValue([4,8,2,9,10], multiplyArray([4,8,2,9,10]), '5760');
+testCaseCheckValue([15,210,32], multiplyArray([15,210,32]), '100800');
+// Test case 2
+testCaseOutputMustBeString([1,2,3], multiplyArray([1,2,3]), '6');
+testCaseOutputMustBeString([4,8,2,9,10], multiplyArray([4,8,2,9, 10]),'5760');
+testCaseOutputMustBeString([15,210,32], multiplyArray([15,210,32]), '100800');
